19cef180f66e639cdeea90f8b3fb33b73d381010
dongchuan1994 <873555954@qq.com>
2020-12-09 13:28:25 +0800

    pycharm

--- all.py
+++ all.py
@@ -5,4 +5,4 @@ sys.path.append(p)
 from common.public import filePath,mkdir,sendEmail
 
 if __name__ == '__main__':
-    pytest.main(['--alluredir', 'allureReport/allure'])
+    pytest.main(['--alluredir', 'allure-results/allure'])
