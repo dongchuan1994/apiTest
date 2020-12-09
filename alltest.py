bfe41d46256970824c19f0db3420389a943c927e
dongchuan1994 <873555954@qq.com>
2020-12-09 12:56:14 +0800

    pycharm

--- alltest.py
+++ alltest.py
@@ -1,5 +1,7 @@
 import pytest,time,os,sys,shutil
 sys.path.append("C:\\Users\\DONGCHUAN\\AppData\\Roaming\\Python\\Python37\\site-packages")
+p = os.path.join(os.path.dirname(__file__))
+sys.path.append(p)
 from common.public import filePath,mkdir,sendEmail
 
 
